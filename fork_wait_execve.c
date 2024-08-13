58e1d6c4034e4af228862cca5c8319b55268a417
Xdamix1991 <bourichalamine1@gmail.com>
2024-08-13 17:15:27 +0200

    fork_wait_execve.c

---
+++ fork_wait_execve.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void)
+
+{
+	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
+	char *envp[] = {NULL};
+	pid_t pid;
+	int i;
+	int statut;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		printf("pid: %d\n", pid);
+
+		if (pid == -1)
+		{
+			perror("faillure");
+			exit(EXIT_FAILURE);
+		}
+
+		if (pid == 0)
+		{
+			execve(argv[0], argv, envp);
+
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+
+		else
+
+		{
+			wait(&statut);
+			pid_t child_pid = wait(&statut);
+			printf("staut is %d\n", child_pid);
+		}
+	}
+	return (0);
+}
