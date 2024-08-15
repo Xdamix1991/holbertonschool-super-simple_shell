ed84cf561a500d3b811b63d5289bcecc87b256e2
Xdamix1991 <bourichalamine1@gmail.com>
2024-08-15 12:35:31 +0200

    Path_current.c

---
+++ Path_current.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#define BUFFER_SIZE 1024
+
+char *get_current_path(void)
+{
+
+	char *path = malloc(BUFFER_SIZE);
+
+	if (path == NULL)
+	{
+		perror("memory allocation");
+		exit(EXIT_FAILURE);
+	}
+
+	if (getcwd(path, BUFFER_SIZE) == NULL)
+	{
+		perror("getcwd");
+		fprintf(stderr, "directory not found\n");
+		free(path);
+		exit(EXIT_FAILURE);
+	}
+
+	return (path);
+}
+
+int main(void)
+{
+	char *p;
+
+	p = get_current_path();
+	printf("current PATH : %s\n", p);
+	free(p);
+	return (0);
+}
